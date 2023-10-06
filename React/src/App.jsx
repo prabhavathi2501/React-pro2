8d9f358984b63cc6f6fc0cd895ec036a56302091
prabhavathi2501 <prabhashok2501@gmail.com>
2023-10-06 14:39:40 +0530

    react project 2

---
+++ React/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Sidebar from './components/Sidebar'
+import Dashboard from './components/Dashboard'
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+
+function App() {
+  return <>
+  <div className='wapper'>
+  
+    <Sidebar/>
+   
+
+</div>
+
+  </>
+}
+
+export default App
---
+++ React/src/components/Sidebar.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+
+function sidebar() {
+  return <>
+    <ul className="navbar-nav bg-light sidebar sidebar-light accordion" id="accordionSidebar">
+
+
+<li className="nav-item active ">
+    <a className="nav-link" href="javascript(void)">
+    <i className="fa-solid fa-grid-horizontal" ></i>
+    {/* <i className="fa-solid fa-grid-horizontal" style={{color: "#155fe0",}} /> */}
+        <h3>Notes App</h3></a>
+</li>
+
+
+
+
+
+<li className="nav-item active ">
+    <a className="nav-link" href="javascript(void)">
+    <i className="fa-solid fa-grid-horizontal" ></i>
+    {/* <i className="fa-solid fa-grid-horizontal" style={{color: "#155fe0",}} /> */}
+        <h3>Notes app</h3></a>
+</li>
+
+
+
+</ul>
+
+  </>
+}
+
+export default sidebar
