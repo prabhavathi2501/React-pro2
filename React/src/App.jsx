3753c43613867cde4666411298d5e56a54375510
prabhavathi2501 <prabhashok2501@gmail.com>
2023-10-06 14:45:34 +0530

    error upadate

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import Sidebar from './components/Sidebar'
-import Dashboard from './components/Dashboard'
-import {BrowserRouter,Routes,Route} from 'react-router-dom'
+
 
 function App() {
   return <>
